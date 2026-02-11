4bb072afb4a4c24a120556b5353041311e9ea1d1
Joao Pedro <jgtecnologiasauto@gmail.com>
2026-02-11 20:58:16 -0300

    abc

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,13 +1,14 @@
 
 // FLAG DE ATIVAÇÃO DO FIREBASE
 // Mude para true quando as chaves abaixo estiverem preenchidas
-export const FIREBASE_ON = false;
+export const FIREBASE_ON = true;
 
 export const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_PROJETO.firebaseapp.com",
-  projectId: "SEU_PROJETO",
-  storageBucket: "SEU_PROJETO.appspot.com",
-  messagingSenderId: "SEU_SENDER_ID",
-  appId: "SEU_APP_ID"
+  apiKey: "AIzaSyCID7AGwR-tfNsiJIBd0nPfBGE5adLAbwY",
+  authDomain: "train-api-49052.firebaseapp.com",
+  projectId: "train-api-49052",
+  storageBucket: "train-api-49052.firebasestorage.app",
+  messagingSenderId: "1056584302761",
+  appId: "1:1056584302761:web:659d6c4a3692ded2c4a9b8",
+  measurementId: "G-DT7ZYWWZ8E",
 };
